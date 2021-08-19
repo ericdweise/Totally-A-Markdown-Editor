663faaf7e45fbcc66be70a6467884ca224cbbb2f
Eric Weise <ericdweise@gmail.com>
2021-08-18 21:49:21 -0700

    Replace spaces with tabs

--- tame.py
+++ tame.py
@@ -8,74 +8,74 @@ import subprocess
 
 
 SKIP_DIRS = (
-        '.bak',
-        '.git',
-        'assets')
+		'.bak',
+		'.git',
+		'assets')
 
 
 def get_title(path):
-    with open(path, 'r') as f:
-        title = f.readline().strip()
+	with open(path, 'r') as f:
+		title = f.readline().strip()
 
-    while title[0] in ('#', ' '):
-        title = title[1:]
+	while title[0] in ('#', ' '):
+		title = title[1:]
 
-    if not len(title):
-        title = 'Unknown'
+	if not len(title):
+		title = 'Unknown'
 
-    return title
+	return title
 
 
 def recurse(root, depth, string):
-    dirs = []
-    files = []
-    for i in os.listdir(root):
-        if os.path.isdir(os.path.join(root, i)):
-            dirs.append(i)
-        elif os.path.isfile(os.path.join(root, i)):
-            if not i.endswith('.md'):
-                continue
-            files.append(i)
+	dirs = []
+	files = []
+	for i in os.listdir(root):
+		if os.path.isdir(os.path.join(root, i)):
+			dirs.append(i)
+		elif os.path.isfile(os.path.join(root, i)):
+			if not i.endswith('.md'):
+				continue
+			files.append(i)
 
-    dirs.sort()
-    files.sort()
+	dirs.sort()
+	files.sort()
 
-    for directory in dirs:
-        if directory.startswith('.') or directory.startswith('_') or directory in SKIP_DIRS:
-            continue
-        string += '\n'
-        string += '\t'*depth
-        string += f'<details><summary>{directory}</summary>'
-        string = recurse(os.path.join(root, directory), depth+1, string)
-        string += '\n'
-        string += '\t'*depth
-        string += '<hr>'
-        string += '\n'
-        string += '\t'*depth
-        string += '</details>'
+	for directory in dirs:
+		if directory.startswith('.') or directory.startswith('_') or directory in SKIP_DIRS:
+			continue
+		string += '\n'
+		string += '\t'*depth
+		string += f'<details><summary>{directory}</summary>'
+		string = recurse(os.path.join(root, directory), depth+1, string)
+		string += '\n'
+		string += '\t'*depth
+		string += '<hr>'
+		string += '\n'
+		string += '\t'*depth
+		string += '</details>'
 
-    if len(files):
-        string += '\n'
-        string += '\t'*depth
-        string += '<ul>'
+	if len(files):
+		string += '\n'
+		string += '\t'*depth
+		string += '<ul>'
 
-    for f in files:
-        path = os.path.join(root, f)
-        title = get_title(path)
-        string += '\n'
-        string += '\t'*(depth+1)
-        string += f'<li><a href="{path[1:].replace(".md", ".html")}">{title}</a></li>'
+	for f in files:
+		path = os.path.join(root, f)
+		title = get_title(path)
+		string += '\n'
+		string += '\t'*(depth+1)
+		string += f'<li><a href="{path[1:].replace(".md", ".html")}">{title}</a></li>'
 
-    if len(files):
-        string += '\n'
-        string += '\t'*depth
-        string += '</ul>'
+	if len(files):
+		string += '\n'
+		string += '\t'*depth
+		string += '</ul>'
 
-    return string
+	return string
 
 
 def make_page(title, contents, site_directory):
-    return f'''<!DOCTYPE html>
+	return f'''<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
@@ -93,19 +93,19 @@ def make_page(title, contents, site_directory):
 		</div> <!-- header -->
 		<div id="container" class="clearfix">
 			<div id="contents">
-                <div id="view-menu">
-                    <button id="edit">Edit Markdown</button>
-                    <button id="file">New Note</button>
-                </div> <!-- id="view-menu" -->
-                <div id="edit-menu" class="hidden">
-                    <button id="save">Save</button>
-                    <button id="abort">Abort</button>
-                </div> <!-- id="edit-menu" -->
-                <div id="viewer">
-                    {contents}
-                </div> <!-- id="viewer" -->
-                <div id="editor" contenteditable="true" class="hidden">
-                </div> <!-- id="editor" -->
+				<div id="view-menu">
+					<button id="edit">Edit Markdown</button>
+					<button id="file">New Note</button>
+				</div> <!-- id="view-menu" -->
+				<div id="edit-menu" class="hidden">
+					<button id="save">Save</button>
+					<button id="abort">Abort</button>
+				</div> <!-- id="edit-menu" -->
+				<div id="viewer">
+					{contents}
+				</div> <!-- id="viewer" -->
+				<div id="editor" contenteditable="true" class="hidden">
+				</div> <!-- id="editor" -->
 			</div> <!-- id="contents" -->
 			<div class="spacer"></div>
 			<div id="sidebar">
@@ -121,28 +121,28 @@ def make_page(title, contents, site_directory):
 
 
 if __name__ == '__main__':
-    site_directory = recurse('.', 0, '')
+	site_directory = recurse('.', 0, '')
 
-    for root, _, files in os.walk('.'):
-        for f in files:
-            if not (f.endswith('.md') or f.endswith('.MD')):
-                continue
+	for root, _, files in os.walk('.'):
+		for f in files:
+			if not (f.endswith('.md') or f.endswith('.MD')):
+				continue
 
-            md_file = os.path.join(root, f)
-            html_file = md_file.replace('.md', '.html')
-            print(f'  {md_file} ---> {html_file}')
+			md_file = os.path.join(root, f)
+			html_file = md_file.replace('.md', '.html')
+			print(f'  {md_file} ---> {html_file}')
 
-            title = get_title(md_file)
+			title = get_title(md_file)
 
-            with open(md_file, 'r') as fin:
-                _ = fin.readline()
-                markdown = fin.read()
+			with open(md_file, 'r') as fin:
+				_ = fin.readline()
+				markdown = fin.read()
 
-            contents = pypandoc.convert_text(
-                    markdown,
-                    'html5',
-                    format='md')
-            html = make_page(title, contents, site_directory)
+			contents = pypandoc.convert_text(
+					markdown,
+					'html5',
+					format='md')
+			html = make_page(title, contents, site_directory)
 
-            with open(html_file, 'w') as fp:
-                fp.write(html)
+			with open(html_file, 'w') as fp:
+				fp.write(html)
