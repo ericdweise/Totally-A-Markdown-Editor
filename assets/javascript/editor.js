7027f4cff206d0002467046426241d06fb82ba75
Eric Weise <ericdweise@gmail.com>
2021-08-22 15:00:05 -0700

    Added function to add new note, combined with save.py in ajax.py

--- assets/javascript/editor.js
+++ assets/javascript/editor.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	/*** BUTTONS ***/
 	// Edit Button
 	$('#edit').click(function() {
 		$('#edit-menu').removeClass('hidden');
@@ -14,18 +15,51 @@ $(document).ready(function(){
 		});
 	});
 
-	// var getSelection = function() {
-	// 	if(window.getSelection) {
-	// 		var sel = window.getSelection();
-	// 		if(sel.rangeCount > 0) {
-	// 			return sel.getRangeAt(0);
-	// 		}
-	// 	} else if(document.selection) {
-	// 		return document.selection.createRange();
-	// 	}
-	// 	return null;
-	// };
+	// Add Note Button
+	$('#file').click(function() {
+		// Prompt user for new file
+		var target = prompt('Enter absolute path to new note','');
+		if (target == null || target =='') {
+			alert('ERROR: Invalid target file');
+		}
+		if (!target.endsWith('.md')) {target += '.md';}
+		console.log('New note target: ' + target);
 
+		// Send AJAX
+		var form = new FormData();
+		form.append('action', 'new-note');
+		form.append('target', target);
+		var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
+		xhr.open( 'post', '/htbin/ajax.py', true);
+		xhr.overrideMimeType('text/x-python');
+		xhr.send(form);
+	});
+
+	// Save Button
+	$('#save').click(function() {
+		// Save editor content
+		var target = window.location.pathname.replace(/\.html$/, '.md');
+		var form = new FormData();
+		var data = $('#editor').text();
+		form.append('action', 'save')
+		form.append("data", data); // $('#editor').text().replace(/\n/g, '<br>'));
+		form.append("target", target);
+		console.log('Save note target: ' + target)
+		var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
+		xhr.open( 'post', '/htbin/ajax.py', true );
+		xhr.overrideMimeType('text/x-python');
+		xhr.send(form);
+	});
+
+	// Abort Button
+	$('#abort').click(function() {
+		$('#edit-menu').addClass('hidden');
+		$('#editor').addClass('hidden');
+		$('#view-menu').removeClass('hidden');
+		$('#viewer').removeClass('hidden');
+	});
+
+	/*** IMPLICIT BEHAVIOR ***/
 	// listen for <Enter>, insert "\n" instead of default behavior
 	document.getElementById('editor').addEventListener('keypress', function(e) {
 		if (e.which == 13) { // Listen for <Enter> key
@@ -52,27 +86,5 @@ $(document).ready(function(){
 			selection.addRange(newRange);
 			this.focus();
 		}
-	});
-
-	// Save Button
-	$('#save').click(function() {
-		// Save editor content
-		var mdfile = window.location.pathname.replace(/\.html$/, '.md');
-		var form = new FormData();
-		var data = $('#editor').text();
-		form.append("data", data); // $('#editor').text().replace(/\n/g, '<br>'));
-		form.append("target", mdfile);
-		var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
-		xhr.open( 'post', '/htbin/save.py', true );
-		xhr.overrideMimeType('text/x-python');
-		xhr.send(form);
-	});
-
-	// Abort Button
-	$('#abort').click(function() {
-		$('#edit-menu').addClass('hidden');
-		$('#editor').addClass('hidden');
-		$('#view-menu').removeClass('hidden');
-		$('#viewer').removeClass('hidden');
 	});
 })
---
+++ htbin/ajax.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import cgi
+import os
+import pathlib
+import sys
+
+from build import make_all, make_page, SITE_DIR
+
+# ROOT, _ = os.path.split(pathlib.Path(__file__).parent.absolute())
+
+
+def target_to_path(target):
+	NOT_ALLOWED = ('..', '~', '$')
+
+	for pattern in NOT_ALLOWED:
+		if pattern in target:
+			raise Exception('Path contains "..".')
+
+	assert(target.endswith('.md') or target.endswith('.MD'))
+
+	while target[0] == '/':
+		target = target[1:]
+
+	return target
+
+
+def save_note(target, data):
+	assert(target is not None)
+	path = target_to_path(target)
+	# assert(os.path.isfile(path))
+
+	if not data.endswith('\n'):
+		data += '\n'
+
+	if path:
+		with open(path,'w') as fp:
+			fp.write(data)
+
+	make_page(path, SITE_DIR)
+
+
+def new_note(target):
+	path = target_to_path(target)
+	assert(not os.path.exists(path))
+	assert(path.endswith('.md') or path.endswith('.MD'))
+
+	root, _ = os.path.split(path)
+	if (root) and (not os.path.exists(root)):
+		os.makedirs(root)
+
+	contents = 'New Note\n\n# Welcome\nAdd your thoughts here.\n'
+	with open(path, 'w') as fp:
+		fp.write(contents)
+
+	make_page(path, SITE_DIR)
+
+
+if __name__ == '__main__':
+	form = cgi.FieldStorage()
+	action = form.getvalue('action', None)
+
+	if action == 'save':
+		target = form.getvalue('target', None)
+		data = form.getvalue('data', None).replace('\r\n', '\n')
+		save_note(target, data)
+
+	elif action == 'new-note':
+		target = form.getvalue('target', None)
+		new_note(target)
+
+	else:
+		raise(Exception(f'Invalid action: "{action}"'))
