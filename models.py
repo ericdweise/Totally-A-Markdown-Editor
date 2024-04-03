b28d962f026d567253cec495a4c4e7689565d812
Eric Weise <ericdweise@gmail.com>
2024-04-02 22:52:39 -0600

    Tabs to spaces

--- models.py
+++ models.py
@@ -1,31 +1,31 @@
 from flask_login import UserMixin
 from sqlalchemy import (
-	Column,
-	Integer,
-	String,
+    Column,
+    Integer,
+    String,
 )
 from werkzeug.security import (
-	generate_password_hash,
-	check_password_hash,
+    generate_password_hash,
+    check_password_hash,
 )
 
 from database import Base
 
 
 class User(Base, UserMixin):
-	__tablename__ = "users"
-	id = Column(Integer, primary_key=True)
-	username = Column(String(50), unique=True)
-	hashpass = Column(String(120), unique=True)
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    hashpass = Column(String(120), unique=True)
 
-	def __init__(self, username=None):
-		self.username = username
+    def __init__(self, username=None):
+        self.username = username
 
-	def __repr__(self):
-		return f"<User {self.username!r}>"
+    def __repr__(self):
+        return f"<User {self.username!r}>"
 
-	def set_password(self, password):
-		self.hashpass = generate_password_hash(password)
+    def set_password(self, password):
+        self.hashpass = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.hashpass, password)
+    def check_password(self, password):
+        return check_password_hash(self.hashpass, password)
