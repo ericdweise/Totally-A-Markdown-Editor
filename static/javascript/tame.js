2a3677f6177cc279b3851855464c18add9be0e9a
Eric Weise <ericdweise@gmail.com>
2025-07-20 20:41:34 -0600

    Javascript remove unnecessary function buildTOC

--- static/javascript/tame.js
+++ static/javascript/tame.js
@@ -10,48 +10,6 @@ $.fn.loadSiteDir = function() {
 	xhr.send();
 };
 
-$.fn.buildTOC = function() {
-	var toc_arr = [];
-
-	function build_toc(item) {
-		function add_toc_item(header_item, depth) {
-			let url = new URL('#' + header_item.id, window.location);
-			var link = document.createElement("a");
-			link.href = url.toString();
-			link.text = header_item.innerText;
-
-			var par = document.createElement("p");
-			par.className = 'toc-item';
-			par.appendChild(link);
-
-			if (depth > 1) {
-				par.style.marginLeft = depth-1 + 'ch';
-			}
-
-			document.getElementById('note-toc').appendChild(par);
-		}
-
-		if (item.localName != null && item.localName.length == 2 && item.localName.charAt(0) == 'h' && item.localName.charAt(1) >= '1' && item.localName.charAt(1) <= '6') {
-			add_toc_item(item, item.localName.charAt(1));
-		}
-	}
-
-    let targetDiv = document.getElementById('note-toc');
-
-    // Clear current ToC
-    targetDiv.innerHTML = "";
-
-    // Add new ToC
-	var hdr = document.createElement("p");
-    hdr.classList.add("sec-title");
-	hdr.textContent = "Contents";
-	targetDiv.appendChild(hdr);
-
-	const vwr = $('#note-rendered');
-	vwr[0].childNodes.forEach(build_toc);
-};
-
-
 $.fn.editNote = function() {
     let url = new URL(window.location.href);
     url.pathname = 'edit';
@@ -121,6 +79,5 @@ $.fn.abortEdit = function() {
 $(document).ready(function() {
     if (document.location.pathname == '/view') {
         $.fn.loadSiteDir();
-        $.fn.buildTOC();
     }
 })
